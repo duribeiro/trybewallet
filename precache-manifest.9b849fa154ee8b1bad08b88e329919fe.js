88caf05d7ffe28a7804e10b431bfd44c3881ba35
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-30 17:39:50 -0400

    Updates

---
+++ precache-manifest.9b849fa154ee8b1bad08b88e329919fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f33255371d6fb383cf4dc2054b9dd42",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "bf69c3221ae69de1544a",
+    "url": "/trybewallet/static/css/main.e6f51bc1.chunk.css"
+  },
+  {
+    "revision": "e4719d24d3ccee3412e3",
+    "url": "/trybewallet/static/js/2.51134b98.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.51134b98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf69c3221ae69de1544a",
+    "url": "/trybewallet/static/js/main.7d3d3e0e.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  }
+]);
